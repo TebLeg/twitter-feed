df0a647a90d13c9121e9cb5226654764c1d03a2c
USER\A100286 <tebogolegoabe@discoverybank.co.za>
2018-03-10 15:16:35 +0200

    Assert test results correctly.

--- src/test/java/za/co/application/TwitterFeedApplicationTests.java
+++ src/test/java/za/co/application/TwitterFeedApplicationTests.java
@@ -4,57 +4,78 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 import za.co.application.twitterfeed.FeedService;
-
 import java.io.File;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 //@SpringBootTest
 public class TwitterFeedApplicationTests {
 
 	@Test
-	public void contextLoads() {
-	}
-
-	@Test(expected = IllegalArgumentException.class)
 	public void invalid_number_of_arguments_failure() throws Exception {
 
 		String[] args = new String[] {"user.txt"};
-		new FeedService().execute(args);
-
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void invalid_user_filename_failure() throws Exception {
-
-		String[] args = new String[] {"user.txt", "tweet.txt"};
-		new FeedService().execute(args);
-
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void invalid_tweet_filename_failure() throws Exception {
-
-		String[] args = new String[] {"user.txt", "twee.txt"};
-		new FeedService().execute(args);
-
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void user_filename_does_not_exist_failure() throws Exception {
-
-		String[] args = new String[] {"files/user.txt", "files/tweet.txt"};
-		new FeedService().execute(args);
+		try {
+			new FeedService().execute(args);
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "Number of arguments is invalid, there should be 2");
+		}
 
 	}
 
 	@Test
-	public void tweet_filename_does_not_exist_failure() throws Exception {
+	public void invalid_user_filename_failure() throws Exception {
+
+		String[] args = new String[] {"user.txt", "tweet.txt"};
+		try {
+			new FeedService().execute(args);
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "File user.txt not found.");
+		}
+
+	}
+
+	@Test
+	public void invalid_tweet_filename_failure() throws Exception {
+
+		ClassLoader classLoader = this.getClass().getClassLoader();
+		File userFile = new File(classLoader.getResource("user.txt").getFile());
+
+		String[] args = new String[] {userFile.getAbsolutePath(), "twee.txt"};
+
+		try {
+			new FeedService().execute(args);
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "File twee.txt not found.");
+		}
+
+	}
+
+	@Test
+	public void user_filename_does_not_exist_failure() throws Exception {
+
+		String[] args = new String[] {"files/user.txt", "files/tweet.txt"};
+
+		try {
+			new FeedService().execute(args);
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "File files/user.txt not found.");
+		}
+
+	}
+
+	@Test
+	public void tweet_filename_does_not_exist_success() throws Exception {
 
 		ClassLoader classLoader = this.getClass().getClassLoader();
 		File file = new File(classLoader.getResource("user.txt").getFile());
 
 		String[] args = new String[] {file.getAbsolutePath(), "files/tweet.txt"};
-		new FeedService().execute(args);
+		FeedService service = new FeedService();
+		service.execute(args);
+
+		String expected = "File " + args[1] + " not found." + System.getProperty("line.separator");
+		assertEquals(expected, service.printErrorList());
 
 	}
 
@@ -66,33 +87,44 @@ public class TwitterFeedApplicationTests {
 		File tweetFile = new File(classLoader.getResource("nonascii/tweet.txt").getFile());
 
 		String[] args = new String[] {userFile.getAbsolutePath(), tweetFile.getAbsolutePath()};
-		new FeedService().execute(args);
+		FeedService service = new FeedService();
+		service.execute(args);
+
+		String expected = "Illegal pattern in user line: Joe. Line skipped." + System.getProperty("line.separator") +
+				"Text is not in ASCII format: R��al follows Alan. Line skipped." + System.getProperty("line.separator");
+		assertEquals(expected, service.printErrorList());
 
 	}
 
 	@Test
-	public void parse_users_file_skip_invalid_format_failure() {
+	public void parse_users_file_skip_invalid_format() {
 		ClassLoader classLoader = this.getClass().getClassLoader();
 		File userFile = new File(classLoader.getResource("invaliduser/user.txt").getFile());
 		File tweetFile = new File(classLoader.getResource("invaliduser/tweet.txt").getFile());
 
 		String[] args = new String[] {userFile.getAbsolutePath(), tweetFile.getAbsolutePath()};
-		new FeedService().execute(args);
+		FeedService service = new FeedService();
+		service.execute(args);
+
+		String expected = "Illegal pattern in user line: Alan following Martin. Line skipped." + System.getProperty("line.separator") +
+				"Illegal pattern in user line: Joe this is wrong. Line skipped." + System.getProperty("line.separator");
+		assertEquals(expected, service.printErrorList());
 	}
 
 	@Test
-	public void parse_tweets_file_skip_invalid_format_failure() {
+	public void parse_tweets_file_skip_invalid_format() {
 		ClassLoader classLoader = this.getClass().getClassLoader();
 		File userFile = new File(classLoader.getResource("invalidtweet/user.txt").getFile());
 		File tweetFile = new File(classLoader.getResource("invalidtweet/tweet.txt").getFile());
 
 		String[] args = new String[] {userFile.getAbsolutePath(), tweetFile.getAbsolutePath()};
-		new FeedService().execute(args);
-	}
+		FeedService service = new FeedService();
+		service.execute(args);
 
-	@Test
-	public void parse_tweets_file_invalid_number_of_characters_failure() {
-
+		String expected = "Invalid tweet line pattern: Ward! There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.. Line skipped." + System.getProperty("line.separator") +
+				"Invalid tweet line pattern: Alan< If you have a procedure with 10 parameters, you probably missed some.. Line skipped." + System.getProperty("line.separator") +
+				"Invalid tweet line pattern: Alan This is missing the > character after the name.. Line skipped." + System.getProperty("line.separator");
+		assertEquals(expected, service.printErrorList());
 	}
 
 	@Test
@@ -102,21 +134,11 @@ public class TwitterFeedApplicationTests {
 		File tweetFile = new File(classLoader.getResource("tweet.txt").getFile());
 
 		String[] args = new String[] {userFile.getAbsolutePath(), tweetFile.getAbsolutePath()};
-		new FeedService().execute(args);
+		FeedService service = new FeedService();
+		service.execute(args);
+
+		String expected = "There are no warnings or errors.";
+		assertEquals(expected, service.printErrorList());
 	}
 
-	@Test
-	public void parse_ascii_files_success() {
-
-	}
-
-	@Test
-	public void display_twitter_feed_failure() {
-
-	}
-
-	@Test
-	public void display_twitter_feed_success() {
-
-	}
 }
