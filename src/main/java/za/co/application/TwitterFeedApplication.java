807e2c9e07e6bfd4e7ef398fd5395ea418583cb7
USER\A100286 <tebogolegoabe@discoverybank.co.za>
2018-03-12 12:25:53 +0200

    Executor bean to be used by the async task.

--- src/main/java/za/co/application/TwitterFeedApplication.java
+++ src/main/java/za/co/application/TwitterFeedApplication.java
@@ -3,9 +3,14 @@ package za.co.application;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import za.co.application.twitterfeed.FeedService;
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class TwitterFeedApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -16,5 +21,16 @@ public class TwitterFeedApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		new FeedService().execute(args);
+	}
+
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("TwitterFeed-");
+		executor.initialize();
+		return executor;
 	}
 }
